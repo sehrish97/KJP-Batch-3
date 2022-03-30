8bd5c7ba88c6ebba1fc7b5f5f12e21869327b55d
sehrish97 <mishi3231@gmail.com>
2022-03-30 05:15:07 -0700

    random password generator

--- Lab5.js
+++ Lab5.js
@@ -7,13 +7,12 @@
 // .then()
 // .then()
 function generatepassword(){
-var chars = "0123456789";
  var passwordLength = prompt('enter your password length')
  var password = "";
  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
+    var randomNumber = Math.floor(Math.random() * 10);
+    password += randomNumber
    }
-   document.getElementById('password').value=password
+    document.getElementById('password').value=password
 }
    
